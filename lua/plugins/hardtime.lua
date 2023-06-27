94aed82d6e857197be5604aeb008844360ccfa79
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2023-06-27 21:55:44 +0200

    initial import

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+-- hardtime.lua
+--
+-- A Neovim plugin helping you establish good command workflow and habit
+
+return {
+  "m4xshen/hardtime.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    --
+    -- https://github.com/m4xshen/hardtime.nvim#readme
+  },
+}
